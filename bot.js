8ff7b17d7a607196d5c0c0742f61168624928052
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-26 03:03:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,62 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const adminprefix = "--"
-const prefix = "!"
-client.on("message", message => {
+const adminprefix = "-"
+const prefix = "+"
 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+
+
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix+'hbc')) {	
+   const embed = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)   
+.setColor('RANDOM')
+        .setDescription(`**
+		
+          Commands. 
+${prefix}1bc - برودكاست للجميع
+${prefix}2bc - برودكاست بريئاكشن 
+${prefix}3bc - برودكاست للاون لاين 
+${prefix}bcrole - برودكاست لرتبة معينة 
+**`)
+    message.author.send(embed)
+message.channel.send(":white_check_mark: | Check Your DM تم الأرسال بلخاص")
+    }
+});  
+
+
+client.on('message', message => {
+if (message.author.id === client.user.id) return;
+if (message.guild) {
+let embed = new Discord.RichEmbed()
+let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + '1bc') {
+if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+if (!args[1]) {
+return;
+}
+  message.guild.members.forEach(m => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return;
+      var bc = new Discord.RichEmbed()
+      .addField('# | الرسالة ', args)
+      .setThumbnail(message.guild.iconURL)
+      .setColor('RANDOM')
+      m.sendMessage(args)
+  });
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply(":x: **ليس لديك صلاحية للنشر هنا**");
+  const AziRo = new Discord.RichEmbed()   
+  .setColor('RANDOM')
+  message.channel.sendEmbed(AziRo);          
+}
+} else {
+  return;
+}
 });
+
+
 client.on('message', message => {
   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + "1bc")) {
+if(message.content.startsWith(prefix + '2bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
@@ -64,11 +103,11 @@ client.on('message' , message => {
     let args = message.content.split(" ").slice(1);
 
     if(!args[0]) {
-      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
+      message.channel.send("قم بمنشنة الرتبة | ");
         return;
     }
     if(!args[1]) {
-      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
+      message.channel.send("قم بمنشنة الرتبة | ");
         return;
     }
 
@@ -98,6 +137,19 @@ client.on('message' , message => {
 });
 
 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "3bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 client.on('message', function(message) {
@@ -181,21 +233,7 @@ client.login(process.env.BOT_TOKEN);
     }
 });
 
-client.on('message', message => {
-    if(message.content.startsWith(prefix+'bchelp')) {
-   const embed = new Discord.RichEmbed()
-.setColor('RANDOM')
-        .setDescription(`**
-		
-          Commands. 
-${prefix}bc - برودكاست للجميع
-${prefix}1bc - برودكاست بريئاكشن 
-${prefix}bcrole - برودكاست لرتبة معينة 
-**`)
-    message.author.send(embed)
-message.channel.send(":white_check_mark: | Check Your DM تم الأرسال بلخاص")
-    }
-});  
+
 
 
 client.login(process.env.BOT_TOKEN);
