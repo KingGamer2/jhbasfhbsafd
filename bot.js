1f237d10ca8dc6493ac1de9fc479e8c8918069bd
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-26 06:21:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,29 @@ msg.delete();
 });
 
 
-
+const jackeo = ['450843030963093524'];
+client.on('message', message => { 
+  if (message.author.bot) return; 
+  if (!message.content.startsWith(prefix)) return;
+    var argresult = message.content.split(` `).slice(1).join(' '); 
+      if (!jackeo.includes(message.author.id)) return; 
+  let command = message.content.split(" ")[0]; 
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command === "say")  { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers?__  **'); 
+          message.delete() 
+    message.channel.sendMessage(args.join(" ")).catch(console.error); 
+  }
+if (command == "emb")    { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers?__  **'); 
+    let say = new Discord.RichEmbed() 
+    .setDescription(args.join("  ")) 
+    .setColor("RANDOM") 
+    message.channel.sendEmbed(say); 
+    message.delete(); 
+  } 
+});
 
 
 
